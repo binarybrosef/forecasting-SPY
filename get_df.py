2b39e6a88d33df93dcb3927bcce1733d9711baf4
binarybrosef <dylan.stadler@gmail.com>
2022-09-11 16:01:53 -0700

    Update get_df.py

--- get_df.py
+++ get_df.py
@@ -1,35 +1,23 @@
 import pandas as pd
 import yfinance as yf
 
-
-# Specify ticker symbols for desired securities
-securities = ['SPY', 'DX-Y.NYB', '^VIX']		
-df_main = pd.DataFrame()
-
-for security in securities:
-
-	# Get dataframe of prices for security
-	df = yf.download(
-		   tickers=security,
-		   interval='2m',
-		   start='2022-04-16',
-		   end='2022-06-14',
-		   prepost=True)
-
-	# Rename close column to security-specific name to enable column joins 
-	df = df.rename(columns={'Close': f'{security}_close'})
-
-	# If df_main has no data, set equal to first df we create. Else, join with existing df_main.
-	if len(df_main) == 0:
-		df_main = df
-	else:
-		df_main = df_main.join(df[f'{security}_close'])
+# Get dataframe of closing prices for SPY
+df = yf.download(tickers='SPY',
+		 interval='1d',
+		 period='2y',
+		 prepost=True)
 
 # Drop all columns other than close prices
-for col in df_main.columns:
-	if 'close' not in col:
-		df_main = df_main.drop(col, axis=1)
+for col in df.columns:
+	if col != 'Close':
+		df.drop(col, axis=1, inplace=True)
 
-# Drop rows with NaN and save to local csv file
-df_main.dropna().to_csv('df.csv')
+# Create the daily arithmatic change in closing price ('delta'), and the log of the daily arithmatic change in closing price ('log_delta')
+df['delta'] = df['Close'].diff()
+df['log_delta'] = np.log10(df['Close']).diff()
 
+# Drop first row as no delta can be computed for it
+df = df[1:]
+
+# Save dataframe to .csv file
+df.to_csv('df_daily_2yr.csv')
